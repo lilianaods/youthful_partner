7984b8d5f3f0bff6b34c16f0fb66c29b68c91cd2
Liliana Silva <lilianaods@outlook.com>
2025-05-18 18:31:15 -0400

    feat: update task

--- api/cruds/task.py
+++ api/cruds/task.py
@@ -1,14 +1,16 @@
-from sqlalchemy.ext.asyncio import AsyncSession
+from typing import List, Tuple, Optional
+
 from sqlalchemy import select
 from sqlalchemy.engine import Result
+from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.models.task as task_model
 import api.schemas.task as task_schema
 
-from typing import List, Tuple
 
-
-async def create_task(db: AsyncSession, task_create: task_schema.TaskCreate) -> task_model.Task:
+async def create_task(
+        db: AsyncSession, task_create: task_schema.TaskCreate
+) -> task_model.Task:
     """
     Create a new task in the database.
     """
@@ -18,10 +20,34 @@ async def create_task(db: AsyncSession, task_create: task_schema.TaskCreate) -> 
     await db.refresh(task)
     return task
 
+
 async def get_tasks_with_done(db: AsyncSession) -> List[Tuple[int, str, bool]]:
-    result: Result = await (
-        db.execute(
-            select(task_model.Task.id, task_model.Task.title, task_model.Done.id.isnot(None).label("done")).outerjoin(task_model.Done)
-        )
+    result: Result = await db.execute(
+        select(
+            task_model.Task.id,
+            task_model.Task.title,
+            task_model.Done.id.isnot(None).label("done"),
+        ).outerjoin(task_model.Done)
     )
     return result.all()
+
+
+async def get_task(db: AsyncSession, task_id: int) -> Optional[task_model.Task]:
+    result: Result = await db.execute(
+        select(task_model.Task).filter(
+            task_model.Task.id == task_id,
+        )
+    )
+    task: Optional[Tuple[task_model.Task]] = result.first()
+    return (
+        task[0] if task is not None else None
+    )  # Take the first element since it is returned in tuple even if there is only one element
+
+
+async def update_task(db: AsyncSession, task_create: task_schema.TaskCreate,
+                      original: task_model.Task):
+    original.title = task_create.title
+    db.add(original)
+    await db.commit()
+    await db.refresh(original)
+    return original
--- api/routers/task.py
+++ api/routers/task.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.cruds.task as task_crud
@@ -21,9 +21,12 @@ async def create_task(task_body: task_schema.TaskCreate, db: AsyncSession = Depe
 
 
 @router.put("/tasks/{task_id}")
-async def update_task(task_id: int, task_body: task_schema.TaskCreate):
-    return task_schema.TaskCreateResponse(id=task_id, **task_body.dict())
+async def update_task(task_id: int, task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)):
+    task = await task_crud.get_task(db, task_id=task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
 
+    return await task_crud.update_task(db, task_body, original=task)
 
 @router.delete("/tasks/{task_id}")
 async def delete_task(task_id: int):
