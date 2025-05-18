b998a9afc3ae5adc1fd8a06fa5c766bbdb780c78
Liliana Silva <lilianaods@outlook.com>
2025-05-18 17:24:17 -0400

    feat: list tasks with done flag

--- api/cruds/task.py
+++ api/cruds/task.py
@@ -1,7 +1,12 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.engine import Result
 
 import api.models.task as task_model
 import api.schemas.task as task_schema
+
+from typing import List, Tuple
+
 
 async def create_task(db: AsyncSession, task_create: task_schema.TaskCreate) -> task_model.Task:
     """
@@ -12,3 +17,11 @@ async def create_task(db: AsyncSession, task_create: task_schema.TaskCreate) -> 
     await db.commit()
     await db.refresh(task)
     return task
+
+async def get_tasks_with_done(db: AsyncSession) -> List[Tuple[int, str, bool]]:
+    result: Result = await (
+        db.execute(
+            select(task_model.Task.id, task_model.Task.title, task_model.Done.id.isnot(None).label("done")).outerjoin(task_model.Done)
+        )
+    )
+    return result.all()
--- api/routers/task.py
+++ api/routers/task.py
@@ -1,30 +1,29 @@
 from typing import List
 
 from fastapi import APIRouter, Depends
-
-import api.schemas.task as task_schema
+from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.cruds.task as task_crud
+import api.schemas.task as task_schema
 from api.db import get_db
-
-from sqlalchemy.ext.asyncio import AsyncSession
 
 router = APIRouter()
 
+
 @router.get("/tasks", response_model=List[task_schema.Task])
-async def list_tasks():
-    return [
-        task_schema.Task(id=1, title="Pick up dry cleaning", done=False),
-        task_schema.Task(id=2, title="Go to the gym", done=True),
-    ]
+async def list_tasks(db: AsyncSession = Depends(get_db)):
+    return await task_crud.get_tasks_with_done(db)
+
 
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
 async def create_task(task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)):
     return await task_crud.create_task(db, task_body)
 
+
 @router.put("/tasks/{task_id}")
 async def update_task(task_id: int, task_body: task_schema.TaskCreate):
     return task_schema.TaskCreateResponse(id=task_id, **task_body.dict())
+
 
 @router.delete("/tasks/{task_id}")
 async def delete_task(task_id: int):
